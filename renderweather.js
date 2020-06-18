e990caec7f2773dc3fc0df118888d64b422e1785
Lanzhou Jiang <lzjiang99@gmail.com>
2020-06-18 20:27:55 +1000

    add png pic file

--- renderweather.js
+++ renderweather.js
@@ -1,4 +1,5 @@
 import _ from "./node_modules/lodash-es/lodash.default.js";
+// import { divide } from "lodash";
 
 export function renderWeather(){
   const weather = JSON.parse(localStorage.getItem("Weather"));
@@ -7,12 +8,21 @@ export function renderWeather(){
   const h2 = document.querySelector("h2")
   const h3 = document.querySelector("h3")
   const ol = document.querySelector("ol")
+  const wrapper = document.querySelector(".wrapper")
 
   function newTime() {
     this.observation_time = weather.location.localtime;
   }
 
+  let pngPic = weather.current.weather_icons;
+  wrapper.insertAdjacentHTML("afterbegin",
+    `<img src="${pngPic}"/>`
+  )
+  
+
+
   _.assign(weather.current, new newTime);
+  delete weather.current.weather_icons
 
 
   // paragraph.innerText = JSON.stringify(weather.current);
